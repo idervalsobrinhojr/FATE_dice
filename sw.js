d3edcd937201be52592c2ad66e6de952155699a9
idervalsobrinhojr <idervalsobrinhojr@gmail.com>
2026-03-27 17:32:37 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "fateDice-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener("activate", event => {
+  const cacheWhitelist = ["baralho-v2"];
+
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (!cacheWhitelist.includes(cache)) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
